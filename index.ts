8d78d6f2dc88cec17325e7226f434802cbddf84f
ShaQay3Q <shaghay3gh@gmail.com>
2024-07-23 18:33:38 +0200

    last commit for today on this project

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
 import express from 'express'
+
+
+// Initializate application
+const app = express(
+    //we can provide configuration with {} here
+);
+
+// Define an endpoint
+
+
+// Run server
